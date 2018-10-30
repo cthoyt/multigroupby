dc85b063858f7ce17999a800b45a99672c716810
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-10-30 23:43:04 +0100

    Add doctests

--- src/multisplitby/__init__.py
+++ src/multisplitby/__init__.py
@@ -46,13 +46,33 @@ def split_by(values: Iterable[F], predicate: Callable[[F], bool]) -> Tuple[Itera
 def multi_split_by(values: Iterable[F], predicates: Iterable[Callable[[F], bool]]) -> Iterable[Iterable[F]]:
     """Split the iterator after the predicate becomes true, then repeat for every remaining iterable.
 
+    Normal usage with one predicate:
+
+    >>> values = range(4)
+    >>> predicates = [lambda x: 2 < x]
+    >>> list(map(list, multi_split_by(values, predicates)))
+    [[0, 1, 2], [3]]
+
+    Normal usage with several predicates:
+
+    >>> values = range(9)
+    >>> predicates = [lambda x: 2 < x, lambda x: 4 < x, lambda x: 7 < x]
+    >>> list(map(list, multi_split_by(values, predicates)))
+    [[0, 1, 2], [3, 4], [5, 6, 7], [8]]
+
     If no values are given, will result in |predicates| + 1 generators, all yielding empty lists.
+
+    >>> values = []
+    >>> predicates = [lambda x: 2 < x, lambda x: 4 < x, lambda x: 7 < x]
+    >>> list(map(list, multi_split_by(values, predicates)))
+    [[], [], [], []]
 
     If no predicates are given, will result in a single generator that yields the original list:
 
-    >>> values = [1, 2, 3, 4]
-    >>> [list(sub_values) for sub_values in multi_split_by(values, [])]
-    >>> [[1, 2, 3, 4]]
+    >>> values = range(4)
+    >>> predicates = []
+    >>> list(map(list, multi_split_by(values, predicates)))
+    [[0, 1, 2, 3]]
     """
     last_value: MaybeF = _Sentinel()
     values = iter(values)
